824cb32ad3af112c655ac4254e2def75843ff0cc
Manikandan8056 <manikandanbala002@gmail.com>
2019-12-02 20:37:44 +0530

    implemented savepoint

--- procedure.sql
+++ procedure.sql
@@ -8,18 +8,22 @@ IN USER_ID INT,
  OUT MESSAGE varchar(100)
  )
 BEGIN
-	DECLARE `exception_occured` BOOLEAN DEFAULT FALSE;
-	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION SET `exception_occured` = TRUE;
+	DECLARE `exception_occured` BOOLEAN DEFAULT TRUE;
+	DECLARE CONTINUE HANDLER FOR SQLEXCEPTION 
+    BEGIN
+		SET MESSAGE = 'Server error, Please try again later..';
+        SET exception_occured = FALSE;
+        rollback TO SP1;
+    END;
     SET autocommit=0;
     START TRANSACTION;
-		
+		SAVEPOINT SP1;  
         INSERT INTO flipkart.order_list (user_id, product_id, quantity, price, discount) values(USER_ID, PRODUCT_ID, QUANTITY, PRICE, DISCOUNT);
+        
+        SAVEPOINT SP2;
         INSERT INTO flipkart.delivery_list (order_id, status) values (ORDER_ID, 'DELIVERABLE');
 	
     IF `exception_occured` THEN
-		SET MESSAGE = 'Server error, Please try again later..';
-		ROLLBACK;
-	ELSE
 		SET MESSAGE = 'Order Success';
 		COMMIT;
 	END IF;
