37590a58b885a56145978475f46714a7ff4a8357
Manikandan8056 <manikandanbala002@gmail.com>
2019-12-03 11:26:21 +0530

    Remove Definer=root in create prcedure line

--- procedure.sql
+++ procedure.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `getorder`(
+CREATE PROCEDURE `getorder`(
 IN USER_ID INT,
  IN PRODUCT_ID INT,
  IN QUANTITY INT,
@@ -13,14 +13,12 @@ BEGIN
     BEGIN
 		SET MESSAGE = 'Server error, Please try again later..';
         SET exception_occured = FALSE;
-        rollback TO SP1;
+        rollback;
     END;
     SET autocommit=0;
     START TRANSACTION;
-		SAVEPOINT SP1;  
+		
         INSERT INTO flipkart.order_list (user_id, product_id, quantity, price, discount) values(USER_ID, PRODUCT_ID, QUANTITY, PRICE, DISCOUNT);
-        
-        SAVEPOINT SP2;
         INSERT INTO flipkart.delivery_list (order_id, status) values (ORDER_ID, 'DELIVERABLE');
 	
     IF `exception_occured` THEN
